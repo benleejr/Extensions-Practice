8e78c1c8fa02cb16ad3b24792d0f4ed91a75f476
Benedict Lee <blee@developedsolutions.com>
2024-02-23 16:53:31 -0600

    Continuing Debugging

--- clickApplyScript.js
+++ clickApplyScript.js
@@ -1,9 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   // Targeting the button by its id
-  const applyButton = document.getElementById('indeedApplyButton');
-  
+    const applyButton = document.getElementById('indeedApplyButton');
+
+    Console.log(applyButton)
+
   // Check if the button exists to avoid errors
-  if (applyButton) {
-    applyButton.click();
+    if (applyButton) {
+        Console.log('Clicked')
+      applyButton.click();
+
   }
 });
